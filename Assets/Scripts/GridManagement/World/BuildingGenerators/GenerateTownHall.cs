f1e5ac5177fc75a1bc62fb0419927d771daa754c
Fureniku <fureniku@gmail.com>
2022-04-30 02:24:52 +0100

    Town Hall modelled

--- Assets/Scripts/GridManagement/World/BuildingGenerators/GenerateTownHall.cs
+++ Assets/Scripts/GridManagement/World/BuildingGenerators/GenerateTownHall.cs
@@ -12,16 +12,16 @@ public class GenerateTownHall : GenerateLargeBuildingBase {
         
         TilePos placePos = new TilePos(startPos.x + startW, startPos.z + startL);
 
-        TilePos junctAE = new TilePos(placePos.x, placePos.z - 1);
-        TilePos carPark1 = new TilePos(placePos.x - 1, placePos.z + 5);
-        TilePos carPark2 = new TilePos(placePos.x + 2, placePos.z + 8);
+        TilePos junctCP = new TilePos(placePos.x - 1, placePos.z + 1);
+        TilePos crossing1 = new TilePos(placePos.x - 1, placePos.z + 5);
+        TilePos crossing2 = new TilePos(placePos.x + 2, placePos.z + 8);
 
-        SetCrossing(junctAE, EnumDirection.WEST);
-        SetCrossing(carPark1, EnumDirection.NORTH);
-        SetCrossing(carPark2, EnumDirection.EAST);
+        SetJunctionIn(junctCP, EnumDirection.NORTH);
+        SetCrossing(crossing1, EnumDirection.NORTH);
+        SetCrossing(crossing2, EnumDirection.EAST);
         
         TilePos scenarioTilePos = new TilePos(placePos.x + 4, placePos.z + 6);
-        Vector3 scenarioPos = new Vector3(scenarioTilePos.GetWorldPos().x, 7.5f, scenarioTilePos.GetWorldPos().z);
+        Vector3 scenarioPos = new Vector3(scenarioTilePos.GetWorldPos().x, 11.5f, scenarioTilePos.GetWorldPos().z-1);
 
         Debug.Log("Setting scenario pos to " +scenarioPos);
         Scenarios.Scenarios.Instance.GetStartPosition().transform.position = scenarioPos;
